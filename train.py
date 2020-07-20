eb7a6540dad48fb287e2602f0ab0173369d0341b
Colton Swingle <colton@Coltons-MacBook-Air.local>
2020-07-20 17:41:30 -0400

    Created new file to process file args

--- train.py
+++ train.py
@@ -3,9 +3,14 @@
 from chemprop.args import TrainArgs
 from chemprop.train import cross_validate
 from chemprop.utils import create_logger
+from chemprop.process_args_file import get_args_list
+import sys
 
 
 if __name__ == '__main__':
+    line_args = get_args_list(sys.argv)
+    sys.argv = sys.argv + line_args
+
     args = TrainArgs().parse_args()
     logger = create_logger(name='train', save_dir=args.save_dir, quiet=args.quiet)
     cross_validate(args, logger)
