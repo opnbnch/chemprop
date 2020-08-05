03d04656ce434cb0fc968cd74facc3d50a2f77f6
Colton Swingle <colton@Coltons-MacBook-Air.local>
2020-08-05 19:34:19 -0400

    Fixed bug with num_preds

--- chemprop/train/make_predictions.py
+++ chemprop/train/make_predictions.py
@@ -101,7 +101,7 @@ def make_predictions(args: PredictArgs, smiles: List[str] = None) -> List[Option
             )
             sum_preds += np.array(model_preds)
         else:
-            sum_batch, sum_var = uncertainty_estimator.UQ_predict(model, sum_batch, sum_var, N)
+            sum_batch, sum_var = uncertainty_estimator.UQ_predict(model, sum_batch, sum_var, N * args.num_preds)
 
     # Ensemble predictions
     if not args.uncertainty:
